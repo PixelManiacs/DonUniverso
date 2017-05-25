8c117499f6f742eb4cdd36c083084a952bdc304b
Torsten Dietl <Torsten.Dietl@codeforcemedia.com>
2017-05-26 00:55:10 +0200

    Added comments, cleaned up the code a bit i.e. removed unnecessary code

--- beansjam_unity/Assets/_Game/Scripts/Bullet.cs
+++ beansjam_unity/Assets/_Game/Scripts/Bullet.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Defines the behavior of a bullet shot by the player or an enemy.
+/// </summary>
 public class Bullet : MonoBehaviour {
 
+    // The GameObject used for the explosion
     public GameObject explodeParticle;
-
+    
     void Start()
     {
+        // Destroy the bullet after 10 seconds
         Destroy(gameObject, 10);
     }
 
+    /// <summary>
+    /// Called if the bullet hits an object.
+    /// Destroys the bullet game object.
+    /// Instantiates the explosion game object and destroys it after 3 seconds.
+    /// </summary>
+    /// <param name="other">The game object the bullet collided with.</param>
     void OnCollisionEnter(Collision other)
     {
         GameObject explosion = Instantiate(explodeParticle, transform.position, Quaternion.identity);
