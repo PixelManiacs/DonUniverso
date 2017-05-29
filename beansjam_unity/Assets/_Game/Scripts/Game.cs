f1a71c108a47cc08676f96c1faa7b329e78245d1
Torsten Dietl <Torsten.Dietl@gmail.com>
2017-05-29 23:31:45 +0200

    Refactoring. Added comments and cleaned code.

--- beansjam_unity/Assets/_Game/Scripts/Game.cs
+++ beansjam_unity/Assets/_Game/Scripts/Game.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The Game script initializes the game by spawning the planets and asteroids.
+/// </summary>
 public class Game : MonoBehaviour {
-	public NameGenerator nameGenerator { get; internal set; }
-	[HideInInspector] public PlanetSpawner planetSpawner;
+
+    // The name generator generates names for the spawned planets.
+	public NameGenerator nameGenerator { 
+        get; 
+        internal set; 
+    }
+
+    // The planet spawner component, used to spawn the planets and asteroids.
+	[HideInInspector] 
+    public PlanetSpawner planetSpawner;
 
 	private void Awake() {
+        // Initialize the name generator and planet spawner
 		Random.InitState("hdmksdfkasdfkjh".GetHashCode());
 		nameGenerator = new NameGenerator();
 		planetSpawner = GetComponent<PlanetSpawner>();
 	}
 
 	private void Start() {
+        // spawn 100 planets
 		for (int i = 0; i < 100; i++)
 			planetSpawner.SpawnPlanet();
 
+        // spawn 50 asteroids
 		for (int i = 0; i < 50; i++)
 			planetSpawner.SpawnAsteroid();
 	}
